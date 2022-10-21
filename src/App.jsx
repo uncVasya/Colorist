265dd350397cae90cfdec767eec459d30ed0ae04
Vasily <89163091443@mail.ru>
2022-10-21 13:39:52 +0300

    0.81

--- src/App.jsx
+++ src/App.jsx
@@ -2,41 +2,70 @@ import React from 'react';
 import chroma from 'chroma-js';
 
 function App() {
-  const arr = new Array(5).fill('#FFFFFF');
+  const oneArr = {
+    color: '#FFFFFF',
+    lock: true,
+  };
+  const arr = new Array(5).fill(oneArr);
   const [cols, setCols] = React.useState(arr);
+
+  function lockUnlock(index) {
+    setCols((state) => state.map((el, i) => {
+      if (i === index) {
+        return {
+          color: el.color,
+          lock: !el.lock,
+        };
+      }
+      return el;
+    }));
+  }
 
   function getRandomColor(index) {
     const letters = '0123456789ABCDEF';
-    let colo = '#';
+    let co = '#';
     for (let i = 0; i < 6; i += 1) {
-      colo += letters[Math.floor(Math.random() * 16)];
+      co += letters[Math.floor(Math.random() * 16)];
     }
-    // setCols((state) => state.map((el, i) => (index === i ? colo : el)));
-    setCols((state) => state.map((el, i) => (index === i ? chroma.random() : el)));
+    setCols((state) => state.map((el, i) => {
+      if (i === index) {
+        return {
+          color: chroma.random(),
+          lock: el.lock,
+
+        };
+      }
+      return el;
+    }));
   }
+
   function getTextColor(i) {
-    const lumin = chroma(cols[i]).luminance();
+    const lumin = chroma(cols[i].color).luminance();
     const colText = lumin > 0.5 ? 'black' : 'white';
     return `${colText}`;
   }
 
-  console.log('render');
+  document.addEventListener('keydown', (event) => {
+    console.log(event.code);
+  });
   return (
     <div className="body">
       {
         cols.map((col, i) => (
           <div
             key={i}
-            style={{ backgroundColor: `${col}` }}
+            style={{
+              backgroundColor: `${col.color}`,
+              color: `${getTextColor(i)}`,
+            }}
             className="col"
           >
             <h2
-              onClick={() => getRandomColor(i)}
-              style={{ сolor: getTextColor(i) }}
+              onClick={() => (col.lock ? getRandomColor(i) : getRandomColor(6))}
             >
-              {`${col}`}
+              {`${col.color}`}
             </h2>
-            <img src="./img/lock.svg" alt="lock" />
+            <img onClick={() => lockUnlock(i)} src={`./img/${col.lock ? 'unlock' : 'lock'}.png`} alt="lock" />
           </div>
         ))
       }
